7ae5d5e7e277236d1f0b3ad17adcbbbbc06a6d11
Asad Memon <asad.lionpk@gmail.com>
2023-10-23 16:52:36 -0700

    minor cleanups

--- index.js
+++ index.js
@@ -1,33 +1,27 @@
 import { runDevTask } from "./dev/lite.mjs";
-import { runPMTask } from "./pm/index.mjs";
 import { runQATask } from "./qa/index.mjs";
 import { DevEnvironment } from "./environment.mjs";
 import path from "path";
 import { getContextFromIssue } from "./common/githubContext.mjs";
-const localEnv = new DevEnvironment(path.join(process.cwd(), 'sample'));
 
+const defaultPath = path.join(process.cwd(), 'sample');
+const localEnv = new DevEnvironment(process.argv[2] || defaultPath);
 const overallTaskContext = await getContextFromIssue();
 
-// const taskList = await runPMTask(overallTaskContext, localEnv);
-
-// console.log("TASK LIST", taskList, taskList.length);
-// // process.exit(0);
-// let taskListSummaries = [];
-// for (const task of taskList) {
-//   const resultSummary = await runDevTask(task, overallTaskContext + "\n\nWork done so far:\n" + taskListSummaries.map(s => `Dev: ${s}`).join("\n\n"), localEnv);
-//   taskListSummaries.push(resultSummary);
-// }
-
+// Run first dev task
 const resultSummary = await runDevTask(overallTaskContext, localEnv);
 
-let bugs = [];
-const resp = await runQATask(overallTaskContext, localEnv, true);
-bugs = resp.bugs;
+// Run QA task to find bugs
+const {bugs} = await runQATask(overallTaskContext, localEnv, true);
 console.log("QA SUMMARY", bugs.length);
+
+// Run dev tasks to fix bugs
 for (const bug of bugs) {
   const rep = `Bug Report:\n${bug} \n\n`;
   await runDevTask(overallTaskContext + "\n\n" + rep, localEnv);
 }
+
 console.log("FINISHED");
 
+// Cleanup and exit any processes that are still running
 localEnv.destroy();
--- localcontext.mjs
+++ localcontext.mjs
@@ -1,7 +1,6 @@
 export const localContext = `
 Context:
 OLD MESSAGES:
-User: Create a basic http server that listens on port 3333
 NEW MESSAGES:
-User: Move the port to a separate constants file
+User: Create a basic http server that listens on port 3333
 `
