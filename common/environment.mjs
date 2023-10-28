66f3b6c4e10307593d21e9ff3c611e21b000d788
Asad Memon <asad.lionpk@gmail.com>
2023-10-27 23:45:33 -0700

    more attempt

--- common/environment.mjs
+++ common/environment.mjs
@@ -33,8 +33,7 @@ export class DevEnvironment {
     try {
       // Start a shell process (e.g., bash)
       console.log("Starting shell");
-      console.log(process.env)
-      this.shellProcess = spawn(process.env.SHELL, [], {
+      this.shellProcess = spawn(process.env.SHELL || '/usr/bin/bash' || '/bin/bash' || 'bash', [], {
         stdio: ['pipe', 'pipe', 'pipe'], // Set up stdio pipes for stdin, stdout, and stderr
         cwd: this.rootPath, // Set the current working directory
       });
